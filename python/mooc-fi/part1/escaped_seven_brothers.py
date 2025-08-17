197ad1030714a5dd7069aac0d8e20eb83582576d
babanaya1 <nuhuhamidu1989@gmail.com>
2025-08-18 03:53:32 +0900

    Finish Seven Brothers exercise with two iterations. One using an escape sequence and the other with several "print" statements.

---
+++ python/mooc-fi/part1/escaped_seven_brothers.py
@@ -0,0 +1,6 @@
+# Program: seven_brothers.py
+# Author:  Nuhu Hamidu
+# Purpose: Print out the names of seven fictional brothers in alphabetical order.
+
+# Printing names using an escape sequence '\n'.
+print("Aapo\nEero\nJuhani\nLauri\nSimeoni\nTimo\nTuomas")
---
+++ python/mooc-fi/part1/seven_brothers.py
@@ -0,0 +1,11 @@
+# Program: seven_brothers.py
+# Author:  Nuhu Hamidu
+# Purpose: Print out the names of seven fictional brothers in alphabetical order.
+
+print("Aapo")
+print("Eero")
+print("Juhani")
+print("Lauri")
+print("Simeoni")
+print("Timo")
+print("Tuomas")
