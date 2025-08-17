5547ab8164a4fd7794b63ae344d1c2a127077bbe
babanaya1 <nuhuhamidu1989@gmail.com>
2025-08-18 04:15:35 +0900

    Add two iterations of Row, Row, Row Your Boat exercise.

---
+++ python/mooc-fi/part1/multiline_literal_row_your_boat.py
@@ -0,0 +1,12 @@
+# Program: row_your_boat.py
+# Author:  Nuhu Hamidu
+# Purpose: Printing out a few lines of the poem - Row, Row, Row Your Boat.
+#          With punctuations and all.
+
+# Another iteration using a multiline string literal.
+print("""
+Row, row, row your boat,
+Gently down the stream.
+Merrily, merrily, merrily, merrily,
+Life is but a dream.
+""")
---
+++ python/mooc-fi/part1/row_your_boat.py
@@ -0,0 +1,9 @@
+# Program: row_your_boat.py
+# Author:  Nuhu Hamidu
+# Purpose: Printing out a few lines of the poem - Row, Row, Row Your Boat.
+#          With punctuations and all.
+
+print("Row, row, row your boat,")
+print("Gently down the stream.")
+print("Merrily, merrily, merrily, merrily,")
+print("Life is but a dream.")
