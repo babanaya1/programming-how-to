c3698c963d10909cdee4b9dc7b06656c66a43b1a
babanaya1 <nuhuhamidu1989@gmail.com>
2025-08-18 04:49:02 +0900

    Practice printing actual quotation marks in a string.

---
+++ python/mooc-fi/part1/print_code.py
@@ -0,0 +1,6 @@
+# Program: print_code.py
+# Author:  Nuhu Hamidu
+# Purpose: Printing out actual quotation marks in a string.
+
+# Print out "print("Hello there!")" maintaining quotations as is.
+print('print("Hello there!")')
