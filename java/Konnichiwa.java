740a49b131027c88bb8cd994619f5cee228a1af7
babanaya1 <nuhuhamidu1989@gmail.com>
2025-08-22 12:54:53 +0900

    Practice compiling and executing a Java program with Konnichiwa.java

---
+++ java/Konnichiwa.java
@@ -0,0 +1,12 @@
+// Program: Konnichiwa.java
+// Author:  Nuhu Hamidu
+// Purpose: Printing a polite greeting.
+
+public class Konnichiwa
+{
+	public static void main(String[] args)
+	{
+		// Print "Konnichiwa, Sekai!" in the terminal window.
+		System.out.println("Konnichiwa, Sekai!");
+	}
+}
