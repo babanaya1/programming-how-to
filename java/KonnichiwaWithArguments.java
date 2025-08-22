4e49b8bbdc2fc3008c1d7eeebfda0cf246e10e9a
babanaya1 <nuhuhamidu1989@gmail.com>
2025-08-22 13:45:52 +0900

    Practice input provision in a basic greeting program with command line arguments

---
+++ java/KonnichiwaWithArguments.java
@@ -0,0 +1,14 @@
+// Program: KonnichiwaWithArguments.java
+// Author:  Nuhu Hamidu
+// Purpose: Print a simple greeting directed at a specific user.
+
+public class KonnichiwaWithArguments
+{
+	public static void main(String[] args)
+	{
+		System.out.print("Konnichiwa, ");
+		System.out.print(args[0]);
+		System.out.println(". Ogenki desu ka?");
+	}
+}
+
